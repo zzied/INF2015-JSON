de1f0a3dab9ea3757e1c5229b736428b47a22677
zzaier <zzaier@CGIMWKSINST601.CEGEPGIM.LCL>
2016-01-30 15:29:34 -0500

    Second Commit

--- src/jsonparsing/Exercice1.java
+++ src/jsonparsing/Exercice1.java
@@ -17,9 +17,13 @@ import net.sf.json.JSONArray;
  * Get the number of CDs contained in the array
  */
 public class Exercice1 {
+    
+    
     public static void main(String[] args) throws Exception {
+        
+        String json2 = utilities.FileReader.loadFileIntoString("json/catalogue.json", "utf-8");
         String json = FileReader.loadFileIntoString("json/catalogue.json", "utf-8");
-        JSONArray livres = JSONArray.fromObject(json);
+        JSONArray livres = JSONArray.fromObject(json2);
         System.out.println("Il y a " + livres.size() + " livre(s) dans le catalogue.");
 
 
---
+++ src/utilities/FileReader.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package utilities;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import org.apache.commons.io.IOUtils;
+
+/**
+ *
+ * @author zzaier
+ */
+public class FileReader {
+
+    public static String loadFileIntoString(String filePath, String fileEncoding) 
+            throws FileNotFoundException, IOException {
+        String fileString = new String(Files.readAllBytes(Paths.get(filePath)), 
+                StandardCharsets.UTF_8);
+
+        return fileString;
+
+    }
+}
